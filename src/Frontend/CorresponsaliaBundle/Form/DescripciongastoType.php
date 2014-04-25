d44c916c3f3d2fb6e0e7bd671f79aa07d8ae7b03
jvalera <jvalera@jvalera-pc.(none)>
2014-04-25 10:39:54 -0430

    jhoan

---
+++ src/Frontend/CorresponsaliaBundle/Form/DescripciongastoType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Frontend\CorresponsaliaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class DescripciongastoType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('descripcion')
+            ->add('partida')
+            ->add('tipogasto')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Frontend\CorresponsaliaBundle\Entity\Descripciongasto'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontend_corresponsaliabundle_descripciongasto';
+    }
+}
